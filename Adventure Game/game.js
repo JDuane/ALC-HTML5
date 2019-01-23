9c9c21b573fc0a5e828048fc9b7f7e1b852bcec5
Jarett <jarett.r259@stu.nebo.edu>
2019-01-23 10:27:05 -0700

    Coded some stuff

---
+++ Adventure Game/game.js
@@ -0,0 +1,18 @@
+// Single line comment
+/*
+
+Multiline comment
+
+*/
+
+// document.write("<h1>I can't wait for <em>summer!</em></h1>");
+
+// alert("warning! Will Robinson...Warning!");
+
+// confirm("Do you like cheese?");
+
+// prompt("What type of cheese do you like?");
+
+var pc = prompt("what is your name?");
+
+confirm("So your name is "+pc);
